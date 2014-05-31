82264f471bdce8fc75d6f6402e66b4c85ca9c10f
Nicolas Klotz <mail@nicolasklotz.de>
2014-05-31 22:34:28 +0200

    Correct ggsave arguments

--- Vote Turnout EP 2014.R
+++ Vote Turnout EP 2014.R
@@ -164,7 +164,7 @@ plot4 <- EU.map +
 
 # Arrange plots side-by-side and save them to disk
 grid.arrange(plot1, plot3, ncol=2) # Absolute plots: vertical positioning
-ggsave("figs/Turnout 2014.png", width=1500, heigth=750, dpi=72)
+ggsave("figs/Turnout 2014.png", width=20, height=10, dpi=72)
 
 grid.arrange(plot2, plot4, ncol=2) # Relative plots: vertical positioning
-ggsave("figs/Change in turnout 2009-2014.png", width=1500, heigth=750, dpi=72)
+ggsave("figs/Change in turnout 2009-2014.png", width=20, height=10, dpi=72)
